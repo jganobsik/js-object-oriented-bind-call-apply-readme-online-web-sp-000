d1c2918a10c124e920ec5ddb005969cab44b0207
Joe Ganobsik <jganobsik@gmail.com>
2019-03-26 02:21:33 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -11,3 +11,16 @@ let person = {
 };
  
 person.greet();
+
+
+function greet() {
+    console.log(`my name is ${this.name}, hi!`);
+}
+ 
+let sally = { name: 'Sally' };
+ 
+greet.call(sally);
+// my name is Sally, hi!
+ 
+greet.apply(sally);
+// my name is Sally, hi!
